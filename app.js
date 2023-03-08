433f2daec9c514d0a44932d9326f19e77d15342a
gismapastalha <talhawizam@gmail.com>
2023-03-08 23:31:55 +0100

    node api terminada

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'users_db'
+    database:'articulosdb'
 });
 //Conexión a la database
 conexion.connect(function(error){
@@ -34,15 +34,64 @@ app.get('/', function(req,res){
     res.send('Ruta INICIO');
 });
 //Mostrar todos los artículos
-app.get('/api/users', (req,res)=>{
-    conexion.query('SELECT * FROM users100', (error,filas)=>{
+app.get('/api/articulos', (req,res)=>{
+    conexion.query('SELECT * FROM articulos', (error,filas)=>{
         if(error){
             throw error;
         }else{
             res.send(filas);
         }
-    })
+    });
 });
+
+//Mostrar todos los artículos
+app.get('/api/articulos/:id', (req,res)=>{
+    conexion.query('SELECT * FROM articulos WHERE id = ?',[req.params.id], (error,fila)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(fila);
+        }
+    });
+});
+
+app.post('/api/articulos', (req,res)=>{
+    let data = {descripcion:req.body.descripcion, precio:req.body.precio, stock:req.body.stock};
+    let sql = "INSERT INTO articulos SET ?";
+    conexion.query(sql, data, function(error,results){
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+app.put('/api/articulos/:id', (req,res)=>{
+    let id =req.params.id;
+    let descripcion = req.body.descripcion;
+    let precio = req.body.precio;
+    let stock = req.body.stock;
+    let sql = "UPDATE articulos SET descripcion = ?, precio = ?, stock = ? WHERE id = ?";
+    conexion.query(sql, [descripcion, precio, stock, id], function(error,results){
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    });
+});
+
+app.delete('/api/articulos/:id', (req,res)=>{
+    conexion.query('DELETE FROM articulos WHERE id = ?',[req.params.id], (error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
+
 
 const puerto = process.env.PUERTO || 3000;
 app.listen(puerto, function(){
